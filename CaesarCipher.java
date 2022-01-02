2b4c4f171487f76240b5dbc4795cd46dad1d9654
Arunprasadh C <52576941+techinessoverloaded@users.noreply.github.com>
2022-01-02 14:47:50 +0530

    Update CaesarCipher.java

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -1,71 +1,72 @@
 import java.util.Scanner;
 public class CaesarCipher
 {
-    public static String LOWER = "abcdefghijklmnopqrstuvwxyz";
-    public static String UPPER = LOWER.toUpperCase();
+    public static String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+    public static String correctCase(String reference,String target)
+    {
+        StringBuilder newRes = new StringBuilder();
+        for(int i=0;i<reference.length();i++)
+        {
+            char refCur = reference.charAt(i);
+            char tarCur = target.charAt(i);
+            if(refCur>=65&&refCur<=90)
+            {
+                tarCur-=32;
+            }
+            newRes.append(tarCur);
+        }
+        return newRes.toString();
+    }
     public static String encrypt(String plainText,int shiftKey)
     {
         StringBuilder builder = new StringBuilder();
-        char[] chars = plainText.toCharArray();
+        String lower = plainText.toLowerCase();
+        char[] chars = lower.toCharArray();
         for(int i=0;i<plainText.length();i++)
         {
             int charPos = 0,keyVal = 0;
             char cur = chars[i];
             char newVal;
-            if(cur>=65&&cur<=90)
+            if(cur==' ')
             {
-                charPos = UPPER.indexOf(cur);
-                keyVal = (charPos+shiftKey)%26;
-                newVal = UPPER.charAt(keyVal);
-                builder.append(newVal);
-            }
-            else if(cur>=97&&cur<=122)
-            {
-                charPos = LOWER.indexOf(cur);
-                keyVal = (charPos+shiftKey)%26;
-                newVal = LOWER.charAt(keyVal);
-                builder.append(newVal);
+                builder.append(" ");
+                
             }
             else
             {
-                builder.append(" ");
+                charPos = ALPHABET.indexOf(cur);
+                keyVal = (charPos+shiftKey)%26;
+                newVal = ALPHABET.charAt(keyVal);
+                builder.append(newVal);
             }
         }
-        return builder.toString();
+        return correctCase(plainText,builder.toString());
     }
     public static String decrypt(String cipherText,int shiftKey)
     {
         StringBuilder builder = new StringBuilder();
-        char[] chars = cipherText.toCharArray();
+        String lower = cipherText.toLowerCase();
+        char[] chars = lower.toCharArray();
         for(int i=0;i<cipherText.length();i++)
         {
             char cur = chars[i];
             int keyVal = 0, charPos = 0;
             char newVal;
-            if(cur>=65&&cur<=90)
-            {
-                charPos = UPPER.indexOf(cur);
-                keyVal = (charPos - shiftKey)%26;
-                if(keyVal<0)
-                keyVal+=26;
-                newVal = UPPER.charAt(keyVal);
-                builder.append(newVal);
-            }
-            else if(cur>=97&&cur<=122)
-            {
-                charPos = LOWER.indexOf(cur);
-                keyVal = (charPos - shiftKey)%26;
-                if(keyVal<0)
-                keyVal+=26;
-                newVal = LOWER.charAt(keyVal);
-                builder.append(newVal);
-            }
-            else
+            if(cur==' ')
             {
                 builder.append(" ");
             }
+            else
+            {
+                charPos = ALPHABET.indexOf(cur);
+                keyVal = (charPos - shiftKey)%26;
+                if(keyVal<0)
+                keyVal+=26;
+                newVal = ALPHABET.charAt(keyVal);
+                builder.append(newVal);
+            }
         }
-        return builder.toString();
+        return correctCase(cipherText,builder.toString());
     }
 	public static void main(String[] args) 
 	{
